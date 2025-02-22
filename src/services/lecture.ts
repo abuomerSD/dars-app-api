2bf352162014adcada8be0abc93e348c611562ec
Eltayeb Ibrahim <tayeb1293@yahoo.com>
2025-02-22 14:06:07 +0200

    dev: service and controller file

---
+++ src/services/lecture.ts
@@ -0,0 +1,21 @@
+import { lecture } from "../models/lecture";
+
+export const saveLecture = async () => {
+    
+}
+
+export const updateLecture = async () => {
+
+}
+
+export const deleteLecture = async () => {
+
+}
+
+export const findLectureById = async () => {
+
+}
+
+export const findAllLectures = async () => {
+
+}
